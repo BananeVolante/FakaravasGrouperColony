e21d019b22e353f4c7328c1cfe9e75a3382bd463
BananeVolante <bananevolante67@gmail.com>
2022-04-23 11:26:44 +0200

    threed controller can noow project and return a vector2<float>, he can also project a vector of points

--- src/3d/ThreeDController.cpp
+++ src/3d/ThreeDController.cpp
@@ -4,14 +4,33 @@
 using namespace Fakarava3d;
 using namespace Eigen;
 
-ThreeDController::ThreeDController(Camera* cam) : cam(cam)
+ThreeDController::ThreeDController(point3D position, float width, float height
+                    , float focal, float screenWidth, float screenHeight) : 
+                    camera(Vector3f(position.x, position.y, position.z), Matrix3f::Identity(), 
+                    width, height, focal, screenWidth, screenHeight)
 {
 
 }
 
-
 Vector2<float> ThreeDController::project(const Vector3f& point)
 {
-    //Vector3f v = cam->getPlane().projection(point);
-    //return Vector2<float>(-v.x(), v.y());
+    Vector2f tmp = camera.project(point);
+    return {tmp[0], tmp[1]};
+}
+
+std::vector<Vector2<float>> ThreeDController::project(const std::vector<Vector3f>& points)
+{
+    std::vector<Vector2<float>> projectedPoints;
+    for(const Vector3f& originalPoint : points)
+    {
+        projectedPoints.push_back(project(originalPoint));
+    }
+    return projectedPoints;
+}
+
+
+
+Camera& ThreeDController::getCamera()
+{
+    return camera;
 }
--- src/3d/ThreeDController.h
+++ src/3d/ThreeDController.h
@@ -7,17 +7,28 @@
 namespace Fakarava3d
 {
     using namespace Fakarava3d;
-class ThreeDController
-{
-private:
-///\brief The camera currently used by this controller
-    Camera* cam;
+    class ThreeDController
+    {
+    public:
+        typedef struct point3D
+        {
+            float x,y,z;
+            float arr[3];
+        } point3D;
 
-public:
-    ThreeDController(Camera* cam);
-    Vector2<float> project(const Vector3f& point);
+    private:
+    ///\brief The camera currently used by this controller
+        Camera camera;
 
-};
+    public:
+        ThreeDController(point3D position, float width, float height
+                        , float focal, float screenWidth, float screenHeight);
+        Vector2<float> project(const Vector3f& point);
+
+        std::vector<Vector2<float>> project(const std::vector<Vector3f>& points);
+
+        Camera& getCamera();
+    };
     
 } 
 
