088b1e125d617c9a6bea4238e8df7559379fe1e5
BananeVolante <bananevolante67@gmail.com>
2022-05-11 20:53:10 +0200

    added some models

--- src/3d/RessourceHandler.cpp
+++ src/3d/RessourceHandler.cpp
@@ -9,6 +9,13 @@ RessourceHandler* RessourceHandler::instance = nullptr;
 void RessourceHandler::load()
 {
     models.insert(std::pair<std::string,Mesh>("lowPolyGrouper",ObjParser::readObject("ressources/lowPolyGrouper.obj")));
+    models.insert(std::pair<std::string,Mesh>("suzanne",ObjParser::readObject("ressources/suzanne.obj")));
+    models.insert(std::pair<std::string,Mesh>("food",ObjParser::readObject("ressources/food.obj")));
+    models.insert(std::pair<std::string,Mesh>("HQ",ObjParser::readObject("ressources/HQ.obj")));
+    models.insert(std::pair<std::string,Mesh>("pheromone",ObjParser::readObject("ressources/pheromone.obj")));
+
+
+
 }
 
 RessourceHandler* RessourceHandler::getInstance()
