b30c91ca408909a1d41a26f10a713b91e7397854
BananeVolante <bananevolante67@gmail.com>
2022-05-13 10:40:17 +0200

    updated ThreeDHQ rotation

--- src/ThreeDHQ.cpp
+++ src/ThreeDHQ.cpp
@@ -6,8 +6,7 @@ void ThreeDHQ::update()
 {
     GrouperHQ::update();
 
-    getMesh().setPosition(Eigen::Vector3f(getPosition()[0], getPosition()[1], 0));
-    getMesh().rotate(Eigen::AngleAxisf(0.03, Eigen::Vector3f::UnitY()));
+    getMesh().rotate(Eigen::AngleAxisf(0.03, Eigen::Vector3f::UnitZ()));
 }
 
 void ThreeDHQ::draw()
@@ -22,5 +21,7 @@ Fakarava3d::InstanciableMesh& ThreeDHQ::getMesh()
 
 ThreeDHQ::ThreeDHQ(Environment *environment, Vector2<float> pos, std::string name) : GrouperHQ(environment, pos, name), mesh(Fakarava3d::RessourceHandler::getInstance()->get("HQ"))
 {
-    mesh.setScale(Eigen::Vector3f::Ones()* getRadius());
+    getMesh().setPosition(Eigen::Vector3f(getPosition()[0], getPosition()[1], 0));
+    getMesh().setRotation(Eigen::AngleAxisf(MathUtils::piDiv2, Eigen::Vector3f::UnitX()).toRotationMatrix()); 
+    getMesh().setScale(Eigen::Vector3f::Ones()* getRadius());
 }
